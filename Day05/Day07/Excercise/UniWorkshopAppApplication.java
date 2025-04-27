7a8e39047970ce8c4801d5d300f26cec03a3cd36
VijeghaJegatheeswaran <139634071+VijeghaJegatheeswaran@users.noreply.github.com>
2025-04-27 09:32:58 +0000

    project updated

---
+++ Day05/Day07/Excercise/UniWorkshopAppApplication.java
@@ -0,0 +1,13 @@
+package com.vau.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniWorkshopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniWorkshopAppApplication.class, args);
+	}
+
+}
